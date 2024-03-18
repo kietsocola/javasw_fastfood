6fa9437f0dfc64cb305a0ed2bd0825a26787c9c6
kietsocola <kietsocola.vankiet27012004@gmail.com>
2024-03-18 15:05:11 +0700

    first commit

---
+++ src/Custom/MyPanelSecond.java
@@ -0,0 +1,14 @@
+package Custom;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class MyPanelSecond extends JPanel {
+	public MyPanelSecond() {
+        super();
+        setBackground(MyColor.SECOND_BAKCGROUND_COLOR);
+//        setMaximumSize(new Dimension(500, 500));
+    }
+}
