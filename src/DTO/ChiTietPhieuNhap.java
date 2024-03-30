4babfaf05e4c289996270167605287a5d9160f2f
tu <nvanhtu2004@gmail.com>
2024-03-30 15:06:18 +0700

    update DTO

--- src/DTO/ChiTietPhieuNhap.java
+++ src/DTO/ChiTietPhieuNhap.java
@@ -6,5 +6,45 @@ public class ChiTietPhieuNhap {
 	private int donGia;
 	private int soLuong;
 	private int thanhTien;
+	public ChiTietPhieuNhap() {
+	
+	}
+	public ChiTietPhieuNhap(int maPN, int maNL, int donGia, int soLuong, int thanhTien) {
+		this.maPN = maPN;
+		this.maNL = maNL;
+		this.donGia = donGia;
+		this.soLuong = soLuong;
+		this.thanhTien = thanhTien;
+	}
+	public int getMaPN() {
+		return maPN;
+	}
+	public void setMaPN(int maPN) {
+		this.maPN = maPN;
+	}
+	public int getMaNL() {
+		return maNL;
+	}
+	public void setMaNL(int maNL) {
+		this.maNL = maNL;
+	}
+	public int getDonGia() {
+		return donGia;
+	}
+	public void setDonGia(int donGia) {
+		this.donGia = donGia;
+	}
+	public int getSoLuong() {
+		return soLuong;
+	}
+	public void setSoLuong(int soLuong) {
+		this.soLuong = soLuong;
+	}
+	public int getThanhTien() {
+		return thanhTien;
+	}
+	public void setThanhTien(int thanhTien) {
+		this.thanhTien = thanhTien;
+	}
 	
 }
