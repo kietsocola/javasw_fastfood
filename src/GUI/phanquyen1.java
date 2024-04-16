367de656774fd3d4d32220f25264bbe6f4332c11
lamkbvn <lamkbvn@gmai.com>
2024-04-16 16:39:45 +0700

    lam

--- src/GUI/phanquyen1.java
+++ src/GUI/phanquyen1.java
@@ -42,13 +42,13 @@ import DTO.phanquyen_DTO;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class NhanVien_GUI extends JFrame {
+public class phanquyen1 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
 	private ArrayList<phanquyen_DTO> ds = new ArrayList<>();
 	private boolean isChange = true;
+	private phanquyen_DAO temp = new phanquyen_DAO();
 
 	/**
 	 * Launch the application.
@@ -57,7 +57,7 @@ public class NhanVien_GUI extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					NhanVien_GUI frame = new NhanVien_GUI();
+					phanquyen1 frame = new phanquyen1();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -69,8 +69,8 @@ public class NhanVien_GUI extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public NhanVien_GUI() {
-		JButton btnThem , btnSua ,btnXoa,btnHuy;
+	public phanquyen1() {
+		JButton btnThem , btnSua ,btnXoa,btnHuy,btnLuu;
 		JComboBox comboBox;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(200, 0, 1000, 800);
@@ -118,56 +118,14 @@ public class NhanVien_GUI extends JFrame {
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(new GridLayout(0, 1, 0, 0));
 		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		panel.add(tabbedPane);
-		
-		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab("Nhân Viên", null, panel_1, null);
-		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.Y_AXIS));
-		
-		JLabel lblNewLabel = new JLabel("QUẢN LÍ NHÂN VIÊN");
-		lblNewLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-		panel_1.add(lblNewLabel);
-		
-		JPanel panel_3 = new JPanel();
-		panel_1.add(panel_3);
-		panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.Y_AXIS));
-		
-		JPanel panel_6 = new JPanel();
-		panel_3.add(panel_6);
-		panel_6.setLayout(new BoxLayout(panel_6, BoxLayout.X_AXIS));
-		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		panel_6.add(lblNewLabel_1);
-		
-		textField = new JTextField();
-		panel_6.add(textField);
-		textField.setColumns(10);
-		
-		JPanel panel_7 = new JPanel();
-		panel_3.add(panel_7);
-		
-		JPanel panel_8 = new JPanel();
-		panel_3.add(panel_8);
-		
-		JPanel panel_9 = new JPanel();
-		panel_3.add(panel_9);
-		
-		JPanel panel_10 = new JPanel();
-		panel_3.add(panel_10);
-		
-		JPanel panel_11 = new JPanel();
-		panel_3.add(panel_11);
-		
-		JPanel panel_4 = new JPanel();
-		panel_1.add(panel_4);
-		
-		JPanel panel_5 = new JPanel();
-		panel_1.add(panel_5);
-		
 		JPanel PhanQuyenPanel = new JPanel();
-		tabbedPane.addTab("Quyền", null, PhanQuyenPanel, null);
+//		tabbedPane.addTab("Quyền", null, PhanQuyenPanel, null);
 		PhanQuyenPanel.setLayout(new GridLayout(3, 1, 0, 0));
+		
+		JPanel panelpanel = new JPanel();
+		panelpanel.add(PhanQuyenPanel);
+		panel.add(panelpanel);
+		panelpanel.setLayout(new GridLayout(1, 0, 0, 0));
 		
 		JPanel PhanQuyenMain = new JPanel();
 		PhanQuyenPanel.add(PhanQuyenMain);
@@ -241,7 +199,7 @@ public class NhanVien_GUI extends JFrame {
 		JPanel btnChucNang = new JPanel();
 		btnChucNang.setBorder(new EmptyBorder(5, 0, 0, 0));
 		PhanQuyenMain.add(btnChucNang);
-		btnChucNang.setLayout(new GridLayout(0, 6, 15, 0));
+		btnChucNang.setLayout(new GridLayout(0, 7, 15, 0));
 		
 		JPanel panel_22 = new JPanel();
 		btnChucNang.add(panel_22);
@@ -250,26 +208,28 @@ public class NhanVien_GUI extends JFrame {
 		comboBox.setPreferredSize(new Dimension(150, 21));
 		panel_15.add(comboBox);
 		btnHuy = new JButton("Hủy");
-		
-		btnThem = new JButton("Thêm quyền");
-		btnSua = new JButton("Sửa quyền");
-		btnXoa = new JButton("Xóa quyền");
+		btnThem = new JButton("Thêm");
+		btnSua = new JButton("Sửa");
+		btnXoa = new JButton("Xóa");
+		btnLuu = new JButton("Lưu");
+		btnLuu.setEnabled(false);
 		btnThem.setEnabled(false);
+		btnXoa.setEnabled(false);
 		btnThem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 					JCheckBox[] ql = {QLNhapHang , QLSanPham , QLNhanVien , QLKhachHang ,QLThongKe } ;
 					
 					btnHuy.setEnabled(true);
+					btnLuu.setEnabled(true);
 					btnThem.setEnabled(false);
 					btnXoa.setEnabled(false);
+					btnSua.setEnabled(false);
 					comboBox.addItem("");
 					comboBox.setEditable(true);
 					comboBox.setSelectedItem("");
 //					comboBox.setSelectedIndex(comboBox.getItemCount());
 					
-					System.out.println(comboBox.getItemCount());
-					btnSua.setEnabled(false);
-					
+					System.out.println("\n" + comboBox.getItemCount());
 					
 					
 					QLNhapHang.setEnabled(true);
@@ -281,12 +241,11 @@ public class NhanVien_GUI extends JFrame {
 					
 			}
 		});
-		btnChucNang.add(btnThem);
 		// Tạo một biểu tượng
-        ImageIcon icon = new ImageIcon("D:\\netbean\\javasw_fastfood\\images\\images.png"); // Thay đổi "icon.png" bằng đường dẫn đến biểu tượng của bạn
+        ImageIcon icon = new ImageIcon("D:\\netbean\\javasw_fastfood\\images\\plus.png"); // Thay đổi "icon.png" bằng đường dẫn đến biểu tượng của bạn
         // Thiết lập kích thước mới cho biểu tượng
         Image img = icon.getImage();
-        Image resizedImg = img.getScaledInstance(40, 40, Image.SCALE_SMOOTH); // Thay đổi 50, 50 thành chiều cao và chiều rộng mong muốn
+        Image resizedImg = img.getScaledInstance(25, 25, Image.SCALE_SMOOTH); // Thay đổi 50, 50 thành chiều cao và chiều rộng mong muốn
         ImageIcon resizedIcon = new ImageIcon(resizedImg);
         btnThem.setIcon(resizedIcon);
 		btnSua.setEnabled(false);
@@ -296,7 +255,6 @@ public class NhanVien_GUI extends JFrame {
         Image resizedImg1 = img1.getScaledInstance(18, 18, Image.SCALE_SMOOTH); // Thay đổi 50, 50 thành chiều cao và chiều rộng mong muốn
         ImageIcon resizedIcon1 = new ImageIcon(resizedImg1);
         btnSua.setIcon(resizedIcon1);
-		btnChucNang.add(btnSua);
 		
 		btnXoa.setEnabled(false);
         ImageIcon icon2 = new ImageIcon("D:\\netbean\\javasw_fastfood\\images\\delete.png"); // Thay đổi "icon.png" bằng đường dẫn đến biểu tượng của bạn
@@ -305,28 +263,47 @@ public class NhanVien_GUI extends JFrame {
         Image resizedImg2 = img2.getScaledInstance(18, 18, Image.SCALE_SMOOTH); // Thay đổi 50, 50 thành chiều cao và chiều rộng mong muốn
         ImageIcon resizedIcon2 = new ImageIcon(resizedImg2);
         btnXoa.setIcon(resizedIcon2);
-		btnChucNang.add(btnXoa);
 		
 		btnHuy.setEnabled(false);
 		btnHuy.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				btnHuy.setEnabled(false);
+				btnLuu.setEnabled(false);
 				btnThem.setEnabled(true);
 				btnSua.setEnabled(true);
 				btnXoa.setEnabled(true);
+				
 				QLNhapHang.setEnabled(false);
 				QLSanPham.setEnabled(false);
 				QLNhanVien.setEnabled(false);
 				QLKhachHang.setEnabled(false);
 				QLThongKe.setEnabled(false);
+				
+				comboBox.setEditable(false);
+				
+				try {
+					ds = temp.getData();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			
+				String[] model = new String[ds.size()];
+				System.out.print(model.length);
+				comboBox.removeAllItems();
+				for(int i = 0 ; i <  model.length ; i++ )
+					comboBox.addItem(ds.get(i).getTenPhanQuyen());
 			}
 		});
+		btnChucNang.add(btnThem);
+		btnChucNang.add(btnSua);
+		btnChucNang.add(btnXoa);
 		btnChucNang.add(btnHuy);
+		btnChucNang.add(btnLuu);
 		
 		comboBox.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
-				phanquyen_DAO temp = new phanquyen_DAO();
 				
 				if(isChange) {
 					try {
@@ -360,15 +337,14 @@ public class NhanVien_GUI extends JFrame {
 					QLThongKe.setSelected(ds.get(index).getThongke());
 					
 	                // Hiển thị giá trị được chọn trong Console (hoặc làm bất kỳ điều gì bạn muốn với giá trị này)
-	                System.out.println("Selected Language: " + selectedLanguage + "\n da duoc edit chua : " + isChange);
+	                System.out.println("Selected Quyen: " + selectedLanguage + "\n da duoc edit chua : " + isChange);
 	            }
 	        });
 		
-		JPanel panel_23 = new JPanel();
-		btnChucNang.add(panel_23);
-		
 		JPanel panel_20 = new JPanel();
 		PhanQuyenPanel.add(panel_20);
+		
+
 
 }
 	}
