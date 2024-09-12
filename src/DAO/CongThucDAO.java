e28ed2e3b0dc76bc5dc4b9b37042c5f7493c76f4
kietsocola <vankiet27012004@gmail.com>
2024-09-12 16:43:49 +0700

    a

--- src/DAO/CongThucDAO.java
+++ src/DAO/CongThucDAO.java
@@ -60,7 +60,7 @@ public class CongThucDAO {
 		return arrCongThuc;
 	}
 	public boolean addCongThuc(CongThuc congThuc) {
-	    boolean result = false;
+		boolean result = false;
 	    if (conDB.openConnectDB()) {
 	        try {
 	            String sql = "INSERT INTO CongThuc(idNguyenLieu, idSanPham, SoLuongDung) VALUES (?, ?, ?)";
@@ -77,6 +77,7 @@ public class CongThucDAO {
 	        }
 	    }
 	    return result;
+	    
 	}
 	public boolean checkExistingCongThuc(CongThuc congThuc) {
 	    boolean result = false;
